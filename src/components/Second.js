55fabcddfa268d80a6ab292cc1312828881b680e
ChriswFoster <chriswfoster@gmail.com>
2018-03-09 16:58:01 -0600

    Huge styling changes, background images, shadows, etc

--- src/components/Second.js
+++ src/components/Second.js
@@ -4,7 +4,48 @@ class cmp extends Component {
   render() {
     return (
       <div className="generalHeight secondBackground">
-        <img className="secondImage" src={require("./images/image_1_26.png")} />
+        <div className="secondFlex2">
+          <img
+            className="secondImage"
+            src={require("./images/image_1_26.png")}
+          />
+          <div className="secondTextBlock">
+            <h1>FEATURES:</h1>
+            <ul>
+              <li>
+                <h3>Wide Range of Compatible Devices</h3>
+              </li>
+              <li>
+                <h3>
+                  Cameras, locks, smoke detectors, window/door sensors,
+                  electronic lockbox, and alarm system are controlled by just
+                  one application.
+                </h3>
+              </li>
+              <li>
+                <h3>
+                  App manages sensor location by each individusal specific
+                  location in home or building.
+                </h3>
+              </li>
+              <li>
+                <h3>
+                  Safety records, indexes of consumption, graphs and statistics
+                  stored in our secure database.
+                </h3>
+              </li>
+              <li>
+                <h3>
+                  Manage all of your connected devices in one place - the palm
+                  of your hand!
+                </h3>
+              </li>
+              <li>
+                <h3>Product support also available.</h3>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
