0592e264efb4c3b44edd45ebcac46ca2f7c5d97d
ChriswFoster <chriswfoster@gmail.com>
2018-03-12 21:05:35 -0500

    Building Third component

--- src/components/Third.js
+++ src/components/Third.js
@@ -6,8 +6,12 @@ class Third extends Component {
 render() {
 return(
 <div className="appheight">
-    Third.
-    <p> test</p>
+<p style={{ fontSize: "2em", color: "white" }}>
+           Installation:
+          </p>
+    <p>We offer affordable installation at competitive rates. </p>
+
+    <p>Our security systems are installed by the best professionals in the business. Security systems will be installed in an unpredictable fashion, giving your security the edge against crime. We offer additional DVR protection, as well as reinforced cabling or wireless packages. Cameras will be placed high, under shelter, away from blinding light, obstruction-free, cables protected, and in obvious places (because an obvious camera can ofter deter wrong doing).</p>
     <p>test</p>
 </div>
 )}
