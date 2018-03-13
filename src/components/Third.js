7072c2cfc1c85eea81347801204500c43038e1f5
ChriswFoster <chriswfoster@gmail.com>
2018-03-12 21:10:13 -0500

    Building Third component

--- src/components/Third.js
+++ src/components/Third.js
@@ -11,8 +11,10 @@ return(
           </p>
     <p>We offer affordable installation at competitive rates. </p>
 
-    <p>Our security systems are installed by the best professionals in the business. Security systems will be installed in an unpredictable fashion, giving your security the edge against crime. We offer additional DVR protection, as well as reinforced cabling or wireless packages. Cameras will be placed high, under shelter, away from blinding light, obstruction-free, cables protected, and in obvious places (because an obvious camera can ofter deter wrong doing).</p>
-    <p>test</p>
+    <p>Our security systems are installed by the leading professionals in the business. Security systems will be installed in an unpredictable fashion, giving your security the edge against crime. We offer additional DVR protection, as well as reinforced cabling or wireless packages. Cameras will be placed high, under shelter, away from blinding light, obstruction-free, cables protected, and in obvious places (because an obvious camera can ofter deter wrong doing).</p>
+    <br />
+    <p>Using the latest hardware, programming languages, and technology, we're able to provide you with a home you can control from almost anywhere in the world! What's better than that? We also provide installation, and we have 24/7 call support as well as online chat support (subscription rates may apply).
+    </p>
 </div>
 )}
 }
