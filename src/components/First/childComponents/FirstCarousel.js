b5fc41f4eb0d39a5ea2e64bece88cbd1f4e1e45d
ChriswFoster <chriswfoster@gmail.com>
2018-03-15 14:00:52 -0500

    Removed 1 carousel and added nukas instead

--- src/components/First/childComponents/FirstCarousel.js
+++ src/components/First/childComponents/FirstCarousel.js
@@ -1,13 +1,34 @@
 import React, {Component} from 'react'
+import Slider from 'react-slick'
 
 import './firstcarousel.css'
 class FirstCarousel extends Component{
-
+constructor(){
+    super()
+    this.state ={
+        settings:{
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+    }
+}
 
     render(){
+        const {settings} = this.state
         return(
         <div>
-  
+                  <Slider {...settings}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+        <div><h3>6</h3></div>
+      </Slider>
+                
             </div>
         )
     }
