c539ead9cb28a840e1f2ed335bfc16ab7087d9f6
Chris <chriswfoster@gmail.com>
2018-03-18 17:06:00 -0500

    Added google map, set location, unstyled

--- src/components/First/First.js
+++ src/components/First/First.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 import FirstCarousel from './childComponents/FirstCarousel'
 import Nav from './childComponents/Nav'
@@ -17,6 +17,14 @@ class First extends Component {
   }
 
   render() {
+    const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+    <GoogleMap
+      defaultZoom={15}
+      defaultCenter={{ lat: 33.081040, lng: -96.677229 }}
+    >
+      {props.isMarkerShown && <Marker position={{ lat: 33.081040, lng: -96.677229 }} />}
+    </GoogleMap>
+  ))
     return (
           <div className="generalHeight topBackground">
       <Nav />
@@ -25,9 +33,16 @@ class First extends Component {
         <h1 style={{marginTop: "0vh", color: "white", textAlign: "center"}}> Ambience Total Security</h1>
           <img src={require('../images/topbannerpic.png')} style={{borderRadius: "25px", height: "36vh"}}/>
           </div>
+        <div>
+          <MyMapComponent
+  isMarkerShown
+  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `40vh`, width: `40vw` }} />}
+  containerElement={<div style={{ height: `50vh`, width: `40vw` }} />}
+  mapElement={<div style={{ height: `40vh`, width: `40vw` }} />}
+/>
         
-        
-
+</div>
 
       </div>
     )
