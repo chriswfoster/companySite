a453b6b1510e2a065aea25a2c4813dc2bd406ff8
Chris <chriswfoster@gmail.com>
2018-03-18 17:37:47 -0500

    Bunch more pictures, working on styling

--- src/components/First/First.js
+++ src/components/First/First.js
@@ -33,7 +33,8 @@ class First extends Component {
         <h1 style={{marginTop: "0vh", color: "white", textAlign: "center"}}> Ambience Total Security</h1>
           <img src={require('../images/topbannerpic.png')} style={{borderRadius: "25px", height: "36vh"}}/>
           </div>
-        <div>
+          <p style={{textAlign: "center", fontSize: "24px"}}>Located in the Dallas / Ft Worth Metroplex</p>
+        <div className="firstBottom">
           <MyMapComponent
   isMarkerShown
   googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
@@ -41,7 +42,12 @@ class First extends Component {
   containerElement={<div style={{ height: `50vh`, width: `40vw` }} />}
   mapElement={<div style={{ height: `40vh`, width: `40vw` }} />}
 />
-        
+    <div className="firstBottomColumnFlex">
+      <p ><b>SECURITY</b> technology and services. <br />
+      And automation at it's best.</p>    
+      <p> Ambience Total Security is a privately owned and operated security firm serving the Dallas / Fort Worth Metroplex. can help you with any Automation or Security System concern.  Whether it's home alarms, closed circuit television, media centers, lighting, temperature control systems, aquaponics, access control, or you name it - we will find the solution that is right for you.  Home and commercial applications.</p>
+      <p> We stay ahead of the game and give you direct insight by providing constant communication from our most qualified and educated security professionals. From shopping centers to gated communities to homes, our security experts are experienced and carry the knowledge, equipment, and skills to meet your security needs fast and effectively. Our approach is client-first, and our grassroots growth lends to our unique ability to meet a wide array of any business’s or home's security and automation needs. We are more than a private security/automation industry— we provide quality customer service from our best-in-class management team that goes the extra mile to meet any challenges with quick, flexible, and decisive resolution. Our promise is that you are left feeling empowered and secure, and we have the intel to make that happen. </p>
+      </div>
 </div>
 
       </div>
