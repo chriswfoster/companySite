4965d2465e04ac47bac06249eb77d9ab83bd3da7
Chris <chriswfoster@gmail.com>
2018-03-18 21:56:59 -0500

    Styling on contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -70,27 +70,29 @@ return(
         <p>PROTECT WHAT MATTERS MOST TO YOU</p>
         <p>Send us a message - let us know how we can help. Ambience Total Security is a rapidly growing company, and offers some of the latest tech on security and automation hardware. We take great pride in our client-first approach and our advanced management structure that makes us the best choice for your needs. Please fill out this form and a Business Development Manager will be in touch with you ASAP!</p>
         </div>
-        <div >
-   
-<p>First Name</p>
-<input placeholder="Your first name." onChange={(e)=> this.fnText(e.target.value) } />
-<p>Last Name</p>
-<input placeholder="Your last name." onChange={(e)=> this.lnText(e.target.value)}/>
-<p>Your Email Address</p>
-<input placeholder="Your email address." onChange={(e)=> this.emailText(e.target.value)} />
-<p>Company Name (optional)</p>
-<input placeholder="Name of the company you represent." onChange={(e)=> this.cnText(e.target.value)}/>
-<p>Phone Number</p>
-<input placeholder="The est number to reach you at." onChange={(e)=> this.pnText(e.target.value)}/>
-<p> Service Location</p>
-<textarea placeholder="City, state, address, or any general information about the location of service." onChange={(e)=> this.slText(e.target.value)}/>
-<p>Special Hardware Requests</p>
-<input placeholder="Hardware brand/model/type" onChange={(e)=> this.hwText(e.target.value)}/>
-<p>Additional Information</p>
-<textarea placeholder="Additional information about your request" onChange={(e)=> this.aiText(e.target.value)}/>
-<button onClick={()=> this.sendMethod(firstNameText, lastNameText, emailAddressText, companyNameText, phoneNumberText, serviceLocationText, hardwareText, additionalInformationText)} style={{backgroundColor: "#003060", color: "white", borderRadius:"5px", padding: "10px"}}>SUBMIT </button>
 
+        <div className="contactFlex">
+        <div>
+   <p>First Name</p>
+<input placeholder="Your first name." onChange={(e)=> this.fnText(e.target.value) } /></div>
+<div>
+<p>Last Name</p>
+<input placeholder="Your last name." onChange={(e)=> this.lnText(e.target.value)}/></div><div>
+<p>Your Email Address</p>
+<input placeholder="Your email address." onChange={(e)=> this.emailText(e.target.value)} /></div><div>
+<p>Company Name (optional)</p>
+<input placeholder="Name of the company you represent." style={{width: "13vw"}} onChange={(e)=> this.cnText(e.target.value)}/></div><div>
+<p>Phone Number</p>
+<input placeholder="The best number to reach you at."  style={{width: "13vw"}} onChange={(e)=> this.pnText(e.target.value)}/></div><div>
+<p> Service Location</p>
+<textarea placeholder="City, state, address, or any general information about the location of service."  style={{width: "15vw", height: "5vh"}}onChange={(e)=> this.slText(e.target.value)}/></div><div>
+<p>Special Hardware Requests</p>
+<input placeholder="Hardware brand/model/type" onChange={(e)=> this.hwText(e.target.value)}  style={{width: "10vw"}}/></div><div>
+<p>Additional Information</p>
+<textarea placeholder="Additional information about your request" onChange={(e)=> this.aiText(e.target.value)} style={{width: "15vw", height: "10vh", marginRight: "5vh"}}/></div>
+<button onClick={()=> this.sendMethod(firstNameText, lastNameText, emailAddressText, companyNameText, phoneNumberText, serviceLocationText, hardwareText, additionalInformationText)} style={{backgroundColor: "#003060", color: "white", borderRadius:"5px", padding: "10px"}}>SUBMIT </button>
 </div>
+
 </div>
 </div>
 )
