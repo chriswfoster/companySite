23ecd14788c3393e007faff1f86c97622d7ac8e0
Chris <chriswfoster@gmail.com>
2018-03-18 21:36:47 -0500

    Set up axios, many on change events, and on submit

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Nav from '../First/childComponents/Nav'
+import axios from 'axios'
 
 import './contact.css'
 
@@ -12,7 +13,7 @@ class Contact extends Component{
             emailAddressText:"",
             companyNameText: "",
             phoneNumberText: "",
-            serviceLocationtext: "",
+            serviceLocationText: "",
             hardwareText: "",
             additionalInformationText: ""
 
@@ -20,18 +21,46 @@ class Contact extends Component{
     }
 
 
-    sendMethod(firstname, lastname){
-        console.log(firstname, lastname)
+    sendMethod(firstname, lastname, email, company, phone, location, hardware, additional){
+        console.log(firstname, lastname, email, company, phone, location, hardware, additional)
+        axios.post('/api/submitcontact/', {
+            firstname, lastname, email, company, phone, location, hardware, additional
+        })
     }
 
 
-
-
+//////////////////////// on change events:
+fnText(val){
+    this.setState({firstNameText: val})
+}
+lnText(val){
+    this.setState({lastNameText: val})
+}
+emailText(val){
+    this.setState({emailAddressText: val})
+}
+cnText(val){
+    this.setState({companyNameText: val})
+}
+pnText(val){
+    this.setState({phoneNumberText: val})
+}
+slText(val){
+    this.setState({serviceLocationText: val})
+}
+hwText(val){
+    this.setState({hardwareText: val})
+}
+aiText(val){
+    this.setState({additionalInformationText: val})
+}
 
 
 
 
 render(){
+    console.log(this.state)
+    const {firstNameText, lastNameText, emailAddressText, companyNameText, phoneNumberText, serviceLocationText, hardwareText, additionalInformationText} = this.state
 return(
 <div className="generalHeight">
     <Nav />
@@ -44,22 +73,22 @@ return(
         <div >
    
 <p>First Name</p>
-<input placeholder="Your first name." name="firstname" type="text" />
+<input placeholder="Your first name." onChange={(e)=> this.fnText(e.target.value) } />
 <p>Last Name</p>
-<input placeholder="Your last name." name="lastname" type="text"/>
+<input placeholder="Your last name." onChange={(e)=> this.lnText(e.target.value)}/>
 <p>Your Email Address</p>
-<input placeholder="Your email address." />
+<input placeholder="Your email address." onChange={(e)=> this.emailText(e.target.value)} />
 <p>Company Name (optional)</p>
-<input placeholder="Name of the company you represent." />
+<input placeholder="Name of the company you represent." onChange={(e)=> this.cnText(e.target.value)}/>
 <p>Phone Number</p>
-<input placeholder="The est number to reach you at." />
+<input placeholder="The est number to reach you at." onChange={(e)=> this.pnText(e.target.value)}/>
 <p> Service Location</p>
-<textarea placeholder="City, state, address, or any general information about the location of service." />
+<textarea placeholder="City, state, address, or any general information about the location of service." onChange={(e)=> this.slText(e.target.value)}/>
 <p>Special Hardware Requests</p>
-<input placeholder="Hardware brand/model/type" />
+<input placeholder="Hardware brand/model/type" onChange={(e)=> this.hwText(e.target.value)}/>
 <p>Additional Information</p>
-<textarea placeholder="Additional information about your request"/>
-<button onSubmit={()=> this.sendMethod(this.state.firstNameText, this.state.lastNameText)} style={{backgroundColor: "#003060", color: "white", borderRadius:"5px", padding: "10px"}}>SUBMIT </button>
+<textarea placeholder="Additional information about your request" onChange={(e)=> this.aiText(e.target.value)}/>
+<button onClick={()=> this.sendMethod(firstNameText, lastNameText, emailAddressText, companyNameText, phoneNumberText, serviceLocationText, hardwareText, additionalInformationText)} style={{backgroundColor: "#003060", color: "white", borderRadius:"5px", padding: "10px"}}>SUBMIT </button>
 
 </div>
 </div>
