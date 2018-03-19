2cf313c1d7e7cf2e9c7b0d2ad4b67a6b1206b906
Chris <chriswfoster@gmail.com>
2018-03-18 20:24:23 -0500

    Some styling done in contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react'
+import Nav from '../First/childComponents/Nav'
+
+import './contact.css'
 
 class Contact extends Component{
     constructor(){
@@ -9,7 +12,19 @@ class Contact extends Component{
 
 render(){
 return(
-<div>
+<div className="generalHeight">
+    <Nav />
+
+
+
+    <div className="contactRowOfColumns">
+
+        <div className="contactTextColumn">
+        <p>PROTECT WHAT MATTERS MOST TO YOU</p>
+        <p>Send us a message - let us know how we can help. Ambience Total Security is a rapidly growing company, and offers some of the latest tech on security and automation hardware. We take great pride in our client-first approach and our advanced management structure that makes us the best choice for your needs. Please fill out this form and a Business Development Manager will be in touch with you ASAP!</p>
+        </div>
+
+<div >
 <p>First Name</p>
 <input placeholder="Your first name." />
 <p>Last Name</p>
@@ -22,11 +37,13 @@ return(
 <input placeholder="The est number to reach you at." />
 <p> Service Location</p>
 <textarea placeholder="City, state, address, or any general information about the location of service." />
-<p placeholder="Hardware brand/model/type">Special Hardware Requests</p>
+<p>Special Hardware Requests</p>
+<input placeholder="Hardware brand/model/type" />
 <p>Additional Information</p>
 <textarea placeholder="Additional information about your request"/>
-
-
+<button style={{backgroundColor: "#003060", color: "white", borderRadius:"5px", padding: "10px"}}>SUBMIT</button>
+</div>
+</div>
 </div>
 )
 }
--- src/components/First/childComponents/Nav.js
+++ src/components/First/childComponents/Nav.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import { Link } from "react-router-dom"
 class Nav extends Component {
 
 
@@ -7,12 +7,12 @@ render() {
 return(
 <div>
 <div className="navFlex">
-          <p className="firstAnimation flashFaderAnimation">SECURITY SERVICES</p>
+            <Link to="/" style={{textDecoration: "none", color: "white"}}><p className="firstAnimation flashFaderAnimation">SECURITY SERVICES</p></Link>
           <p className="secondAnimation flashFaderAnimation">INDUSTRY</p>
           <p className="thirdAnimation flashFaderAnimation">ABOUT US</p>
           <p className="fourthAnimation flashFaderAnimation">INSIGHTS</p>
           <p className="fifthAnimation flashFaderAnimation">OUR WORK</p>
-          <p className="sixthAnimation flashFaderAnimation">CONTACT US</p>
+          <Link to="/contact" style={{textDecoration: "none", color: "white"}}><p className="sixthAnimation flashFaderAnimation">CONTACT US</p></Link>
         </div>
 </div>
 )}
