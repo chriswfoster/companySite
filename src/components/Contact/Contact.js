e8fc857d68fe8480c23b9894d4f7a6e9c120e8fc
ChriswFoster <chriswfoster@gmail.com>
2018-03-20 14:59:33 -0500

    Final settings to Contact cmp

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,118 +1,226 @@
-import React, {Component} from 'react'
-import Nav from '../First/childComponents/Nav'
-import axios from 'axios'
+import React, { Component } from "react"
+import Nav from "../First/childComponents/Nav"
+import axios from "axios"
 
-import './contact.css'
+import "./contact.css"
 
-class Contact extends Component{
-    constructor(){
-        super()
-        this.state = {
+class Contact extends Component {
+  constructor() {
+    super()
+    this.state = {
+      contactQuoteText: "REQUEST A QUOTE",
 
-            contactQuoteText: "REQUEST A QUOTE",
-            
-            //// input text
-            firstNameText: "",
-            lastNameText: "",
-            emailAddressText:"",
-            companyNameText: "",
-            phoneNumberText: "",
-            serviceLocationText: "",
-            hardwareText: "",
-            additionalInformationText: ""
-
-        }
+      //// input text
+      firstNameText: "",
+      lastNameText: "",
+      emailAddressText: "",
+      companyNameText: "",
+      phoneNumberText: "",
+      serviceLocationText: "",
+      hardwareText: "",
+      additionalInformationText: ""
     }
+  }
 
+  sendMethod(
+    firstname,
+    lastname,
+    email,
+    company,
+    phone,
+    location,
+    hardware,
+    additional
+  ) {
+    console.log(
+      firstname,
+      lastname,
+      email,
+      company,
+      phone,
+      location,
+      hardware,
+      additional
+    )
+    axios.post("/api/submitcontact/", {
+      firstname,
+      lastname,
+      email,
+      company,
+      phone,
+      location,
+      hardware,
+      additional
+    })
+  }
 
-    sendMethod(firstname, lastname, email, company, phone, location, hardware, additional){
-        console.log(firstname, lastname, email, company, phone, location, hardware, additional)
-        axios.post('/api/submitcontact/', {
-            firstname, lastname, email, company, phone, location, hardware, additional
-        })
-    }
+  //////////////////////// on change events:
+  fnText(val) {
+    this.setState({ firstNameText: val })
+  }
+  lnText(val) {
+    this.setState({ lastNameText: val })
+  }
+  emailText(val) {
+    this.setState({ emailAddressText: val })
+  }
+  cnText(val) {
+    this.setState({ companyNameText: val })
+  }
+  pnText(val) {
+    this.setState({ phoneNumberText: val })
+  }
+  slText(val) {
+    this.setState({ serviceLocationText: val })
+  }
+  hwText(val) {
+    this.setState({ hardwareText: val })
+  }
+  aiText(val) {
+    this.setState({ additionalInformationText: val })
+  }
 
+  dropDown(val) {
+    val === "contact"
+      ? this.setState({ contactQuoteText: "CONTACT US" })
+      : this.setState({ contactQuoteText: "REQUEST A QUOTE" })
+  }
 
-//////////////////////// on change events:
-fnText(val){
-    this.setState({firstNameText: val})
-}
-lnText(val){
-    this.setState({lastNameText: val})
-}
-emailText(val){
-    this.setState({emailAddressText: val})
-}
-cnText(val){
-    this.setState({companyNameText: val})
-}
-pnText(val){
-    this.setState({phoneNumberText: val})
-}
-slText(val){
-    this.setState({serviceLocationText: val})
-}
-hwText(val){
-    this.setState({hardwareText: val})
-}
-aiText(val){
-    this.setState({additionalInformationText: val})
-}
-
-dropDown(val){
-    val === 'contact' ? this.setState({contactQuoteText: "CONTACT US"}) :
-    this.setState({contactQuoteText: "REQUEST A QUOTE"})
-}
-
-
-render(){
+  render() {
     console.log(this.state)
-    const {firstNameText, lastNameText, emailAddressText, companyNameText, phoneNumberText, serviceLocationText, hardwareText, additionalInformationText} = this.state
-return(
-<div className="generalHeight " style={{backgroundColor: "#e5e3df"}}>
-    <Nav />
-    <div className="contactBanner">
-    <p style={{fontWeight: "bold", fontSize:"2em"}}>{this.state.contactQuoteText}</p>
-    <p style={{marginTop: "-3vh", fontSize: ".8em"}}>Amient Total Security</p>
+    const {
+      firstNameText,
+      lastNameText,
+      emailAddressText,
+      companyNameText,
+      phoneNumberText,
+      serviceLocationText,
+      hardwareText,
+      additionalInformationText
+    } = this.state
+    return (
+      <div className="generalHeight " style={{ backgroundColor: "#e5e3df" }}>
+        <Nav />
+        <div className="contactBanner">
+          <p style={{ fontWeight: "bold", fontSize: "2em" }}>
+            {this.state.contactQuoteText}
+          </p>
+          <p style={{ marginTop: "-3vh", fontSize: ".8em" }}>
+            Ambience Total Security
+          </p>
         </div>
-    <div className="contactRowOfColumns">
-        <div className="contactTextColumn">
-        <p>PROTECT WHAT MATTERS MOST TO YOU</p>
-        <p>Send us a message - let us know how we can help. Ambience Total Security is a rapidly growing company, and offers some of the latest tech on security and automation hardware. We take great pride in our client-first approach and our advanced management structure that makes us the best choice for your needs. Please fill out this form and a Business Development Manager will be in touch with you ASAP!</p>
+        <div className="contactRowOfColumns"  style={{ backgroundColor: "#e5e3df"}}>
+          <div className="contactTextColumn">
+            <p>PROTECT WHAT MATTERS MOST TO YOU</p>
+            <p>
+              Send us a message - let us know how we can help. Ambience Total
+              Security is a rapidly growing company, and offers some of the
+              latest tech on security and automation hardware. We take great
+              pride in our client-first approach and our advanced management
+              structure that makes us the best choice for your needs. Please
+              fill out this form and a Business Development Manager will be in
+              touch with you ASAP!
+            </p>
+          </div>
+
+          <div className="contactFlex">
+            <div>
+              <p>Contact/Quote:</p>
+              <select
+                defaultValue="quote"
+                onChange={e => this.dropDown(e.target.value)}
+              >
+                <option value="contact">Contact</option>
+                <option value="quote">Quote</option>
+              </select>
+            </div>
+            <div>
+              <p>First Name</p>
+              <input
+                placeholder="Your first name."
+                onChange={e => this.fnText(e.target.value)}
+              />
+            </div>
+            <div>
+              <p>Last Name</p>
+              <input
+                placeholder="Your last name."
+                onChange={e => this.lnText(e.target.value)}
+              />
+            </div>
+            <div>
+              <p>Your Email Address</p>
+              <input
+                placeholder="Your email address."
+                onChange={e => this.emailText(e.target.value)}
+              />
+            </div>
+            <div>
+              <p>Company Name (optional)</p>
+              <input
+                placeholder="Name of the company you represent."
+                style={{ width: "15vw" }}
+                onChange={e => this.cnText(e.target.value)}
+              />
+            </div>
+            <div>
+              <p>Phone Number</p>
+              <input
+                placeholder="The best number to reach you at."
+                style={{ width: "15vw" }}
+                onChange={e => this.pnText(e.target.value)}
+              />
+            </div>
+            <div>
+              <p> Service Location</p>
+              <textarea
+                placeholder="City, state, address, or any general information about the location of service."
+                style={{ width: "15vw", height: "5vh" }}
+                onChange={e => this.slText(e.target.value)}
+              />
+            </div>
+            <div>
+              <p>Special Hardware Requests</p>
+              <input
+                placeholder="Hardware brand/model/type"
+                onChange={e => this.hwText(e.target.value)}
+                style={{ width: "15vw" }}
+              />
+            </div>
+            <div>
+              <p>Additional Information</p>
+              <textarea
+                placeholder="Additional information about your request"
+                onChange={e => this.aiText(e.target.value)}
+                style={{ width: "15vw", height: "10vh", marginRight: "5vh" }}
+              />
+            </div>
+            <button
+              onClick={() =>
+                this.sendMethod(
+                  firstNameText,
+                  lastNameText,
+                  emailAddressText,
+                  companyNameText,
+                  phoneNumberText,
+                  serviceLocationText,
+                  hardwareText,
+                  additionalInformationText
+                )
+              }
+              style={{
+                backgroundColor: "#003060",
+                color: "white",
+                borderRadius: "5px",
+                padding: "10px"
+              }}
+            >
+              SUBMIT{" "}
+            </button>
+          </div>
         </div>
-
-        <div className="contactFlex">
-        <div>
-            <p>Contact/Quote:</p>
-        <select defaultValue="quote" onChange={(e)=> this.dropDown(e.target.value)}>
-  <option value="contact">Contact</option>
-  <option value="quote">Quote</option>
-  </select>
-  </div>
-        <div>
-   <p>First Name</p>
-<input placeholder="Your first name." onChange={(e)=> this.fnText(e.target.value) } /></div>
-<div>
-<p>Last Name</p>
-<input placeholder="Your last name." onChange={(e)=> this.lnText(e.target.value)}/></div><div>
-<p>Your Email Address</p>
-<input placeholder="Your email address." onChange={(e)=> this.emailText(e.target.value)} /></div><div>
-<p>Company Name (optional)</p>
-<input placeholder="Name of the company you represent." style={{width: "13vw"}} onChange={(e)=> this.cnText(e.target.value)}/></div><div>
-<p>Phone Number</p>
-<input placeholder="The best number to reach you at."  style={{width: "13vw"}} onChange={(e)=> this.pnText(e.target.value)}/></div><div>
-<p> Service Location</p>
-<textarea placeholder="City, state, address, or any general information about the location of service."  style={{width: "15vw", height: "5vh"}}onChange={(e)=> this.slText(e.target.value)}/></div><div>
-<p>Special Hardware Requests</p>
-<input placeholder="Hardware brand/model/type" onChange={(e)=> this.hwText(e.target.value)}  style={{width: "10vw"}}/></div><div>
-<p>Additional Information</p>
-<textarea placeholder="Additional information about your request" onChange={(e)=> this.aiText(e.target.value)} style={{width: "15vw", height: "10vh", marginRight: "5vh"}}/></div>
-<button onClick={()=> this.sendMethod(firstNameText, lastNameText, emailAddressText, companyNameText, phoneNumberText, serviceLocationText, hardwareText, additionalInformationText)} style={{backgroundColor: "#003060", color: "white", borderRadius:"5px", padding: "10px"}}>SUBMIT </button>
-</div>
-
-</div>
-</div>
-)
-}
+      </div>
+    )
+  }
 }
 export default Contact
---
+++ src/components/OurWork/OurWork.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class OurWork extends Component {
+constructor()
+super(){
+
+}
+
+render() {
+return(
+<div>
+    <div>
+        
+    </div>
+</div>
+)}
+}
+export default OurWork
